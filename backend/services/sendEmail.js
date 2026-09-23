9aa9984648b54a56b9f1597228c02f14b6e6e3dd
Yanis Rezgui <yanisrezgui28@gmail.com>
2026-09-23 23:30:15 +0100

    Added sitemap and robots

--- backend/services/sendEmail.js
+++ backend/services/sendEmail.js
@@ -3,13 +3,14 @@ import { RESEND_API_KEY } from "../config/env.js";
 
 const resend = new Resend(RESEND_API_KEY);
 
-const sendEmail = async ({ to, subject, html }) => {
-  await resend.emails.send({
-    from: "onboarding@resend.dev", // domaine par défaut gratuit
-    to,
-    subject,
-    html
-  });
+const sendEmail = async ({ to, subject, html, replyTo }) => {
+    await resend.emails.send({
+        from: "onboarding@resend.dev",
+        to,
+        subject,
+        html,
+        replyTo
+    });
 };
 
 export default sendEmail;
